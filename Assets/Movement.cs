39381d4ee1f5a87d85e759e595c362ae1b9ae740
Aleyna Türkoğlu <aleynaturkoglu22@gmail.com>
2023-05-21 02:15:06 +0300

    Movement(up,rotation)

---
+++ Assets/Movement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationThrust = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) //GETKEY: basýlý tutulduðu sürece true...
+        {
+            rb.AddRelativeForce(Vector3.up * Time.deltaTime * mainThrust); 
+            //addrelative force çünkü rotasyon deðiþince cismin kendi y'sine göre gitmesini istiyoruz dünyanýn y'sine göre deðil.
+            //Vector3.up = (0,1,0) yazmanýn kýsa yolu.
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+
+        void ApplyRotation(float rotationThisFrame)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * rotationThisFrame);
+        }
+    }
+}
