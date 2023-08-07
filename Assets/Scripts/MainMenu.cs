1cbf2c67bf034a564b7af6ca9852717c5980d7ef
Aleyna Türkoğlu <aleynaturkoglu22@gmail.com>
2023-08-07 23:50:27 +0300

    last commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayButton()
+    {
+
+    }
+
+    void LevelButton()
+    {
+
+    }
+
+    void QuitButton()
+    {
+
+    }
+
+   
+}
